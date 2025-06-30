2eeb3c31d64e56b3a2c9c66e926201ecab84f277
NIGHTMAReS <midogames331@gmail.com>
2025-07-01 00:29:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMMu-QNPL6RlGYdGGQVJLzZqCC_hsLa8I",
+  authDomain: "night-ac2a0.firebaseapp.com",
+  databaseURL: "https://night-ac2a0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "night-ac2a0",
+  storageBucket: "night-ac2a0.appspot.com",
+  messagingSenderId: "202751732517",
+  appId: "1:202751732517:web:5d458d19aac8d7135848cc"
+};
+
 const gallery = document.getElementById("gallery");
 const searchInput = document.getElementById("searchInput");
 const categorySelect = document.getElementById("categorySelect");
